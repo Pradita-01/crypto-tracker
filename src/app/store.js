68f1bea5a385dc26bbcb913a9cbc460b7856a430
Pradita-01 <praditag01@gmail.com>
2025-09-03 23:45:15 +0530

    Delete src/app/store.js

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cryptoReducer from '../features/cryptoslice';
-
-export const store = configureStore({
-  reducer: {
-    crypto: cryptoReducer
-  }
-});
